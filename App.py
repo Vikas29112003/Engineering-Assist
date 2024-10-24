20c0a6abbd3160363a2eb762e86ed53289993a75
vikas_Singh <vikas.singh22@spit.ac.in>
2024-10-24 23:30:01 +0530

    site

--- App.py
+++ App.py
@@ -182,5 +182,5 @@ def upload():
         return jsonify({'error': f'Upload failed: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
